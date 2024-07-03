32a0f974f6979e9dba9b38a8ba1e1d5e54a53337
iacta <thiagobahiavitor@gmail.com>
2024-07-03 18:04:25 -0300

    New Build

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,25 @@
-'use client';
-
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { coming } from "./fonts";
 import { Login } from './login';
 import { getUser } from './dashboard/user';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function checkUser() {
+      await getUser();
+      setLoading(false);
+    }
+    checkUser();
+  }, []);
+
+  if (loading) {
+    return <main>
+
+    </main>
+  }
+
   return (
     <main className={`relative min-h-screen text-black ${coming.className}`}>
       <div className="relative z-10">
