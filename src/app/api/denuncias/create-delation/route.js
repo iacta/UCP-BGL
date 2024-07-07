4624d441e87bc69871addaa38e99661dd2375999
iacta <thiagobahiavitor@gmail.com>
2024-07-07 14:36:58 -0300

    teste

--- src/app/api/denuncias/create-delation/route.js
+++ src/app/api/denuncias/create-delation/route.js
@@ -8,10 +8,10 @@ import path from 'path';
 import { v4 as uuidv4 } from 'uuid'
 import 'dotenv/config'
 import { getUserInfo } from '@/app/dashboard/user';
+import { put } from '@vercel/blob';
 
 export async function POST(request) {
   try {
-    
     const info = await getUserInfo();
     const { reason, images, accused, accusedOrg, reporterOrg, description, staff } = await request.json();
     const reporter = info.nick;
@@ -25,37 +25,35 @@ export async function POST(request) {
         orgRelator: reporterOrg,
         orgAccused: accusedOrg,
         staff: staff,
-        description: description
-      }
-    })
-    await Promise.all(images.map(async (image, index) => {
-      const [mimeType, base64Data] = image.split(';base64,');
-      const extension = mimeType.split('/')[1];
-      const buffer = Buffer.from(base64Data, 'base64');
+        description: description,
+      },
+    });
 
-      const fileName = `${uuidv4()}.${extension}`;
+    const uploadedImageUrls = await Promise.all(
+      images.map(async (image) => {
+        const [mimeType, base64Data] = image.split(';base64,');
+        const buffer = Buffer.from(base64Data, 'base64');
+        const fileName = `${uuidv4()}.${mimeType.split('/')[1]}`;
 
-      const uploadsDir = path.join(process.cwd(), 'public', 'uploads', reporter, result.id.toString());
+        const blob = await put(fileName, buffer, {
+          access: 'public',
+        });
 
-      await fs.promises.mkdir(uploadsDir, { recursive: true });
+        return blob.url;
+      })
+    );
 
-      const filePath = path.join(uploadsDir, fileName);
-
-      await fs.promises.writeFile(filePath, buffer);
-    }));
     const response = await notifyDC(accused, reporter, accusedOrg, reporterOrg, description, reason);
 
     if (response.status === 200) {
-      await prisma.$disconnect()
-
-      return NextResponse.json({ success: true, message: 'Denúncia enviada com sucesso' }, { status: 200 });
+      await prisma.$disconnect();
+      return NextResponse.json({ success: true, message: 'Denúncia enviada com sucesso', imageUrls: uploadedImageUrls }, { status: 200 });
     } else {
-      await prisma.$disconnect()
-
-      return NextResponse.json({ success: true, message: 'Denúncia enviada com sucesso' }, { status: 200 });
+      await prisma.$disconnect();
+      return NextResponse.json({ success: true, message: 'Denúncia enviada com sucesso', imageUrls: uploadedImageUrls }, { status: 200 });
     }
   } catch (error) {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
     console.error('Erro ao criar denuncia:', error);
     return NextResponse.json({ success: false, error: 'Erro interno do servidor.' }, { status: 500 });
   }
