cb2e4cfee317a0c8e813f27315ebc15bd8d05fc8
iacta <thiagobahiavitor@gmail.com>
2024-07-07 15:11:15 -0300

    filter

--- src/app/api/denuncias/images-delations/[reporterId]/[delationId]/route.js
+++ src/app/api/denuncias/images-delations/[reporterId]/[delationId]/route.js
@@ -1,25 +1,20 @@
 import { NextResponse } from 'next/server';
-import { get } from '@vercel/blob';
 import { list } from '@vercel/blob';
 
 export const runtime = 'edge';
 
 export async function GET(request, { params }) {
   const { reporterId, delationId } = params;
-  const { blobs } = await list();
-
-  console.log(blobs);
   try {
-    const images = await get(`denuncias/${reporterId}/${delationId}`);
+    const { blobs } = await list();
 
-    const imagensComURLs = images.map((image) => {
-      return {
-        nome: image.Key.split('/').pop(),
-        url: image.Location
-      };
+    console.log(blobs);
+    const filteredBlobs = blobs.filter(blob => {
+      return blob.pathname.startsWith(`denuncias/${reporterId}/${delationId}`);
+    }).sort((a, b) => {
+      return new Date(b.uploadedAt) - new Date(a.uploadedAt);
     });
-
-    return NextResponse.json(imagensComURLs);
+    return NextResponse.json(filteredBlobs);
   } catch (error) {
     console.error('Erro ao listar imagens:', error);
     throw error;
